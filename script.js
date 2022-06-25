268a10b3c2e88fc638c905f7e07a51738968d02f
Thiagox <ezequiel.thiago.2602@gmail.com>
2022-06-24 21:28:45 -0300

    Funciones del juego realizadas, cambios de estilo en botones y cambios en la estructura del dom

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-let pantalla= document.querySelector("canvas");
-let pincel= pantalla.getContext("2d")
-pincel.fillStyle= "red";
-pincel.strokeStyle = "blue";
-pincel.moveTo(90,130);
-pincel.lineTo(210,130);
-pincel.moveTo(150,130)
-pincel.lineTo(150,30);
-pincel.moveTo(150,30);
-pincel.lineTo(220,30);
-pincel.moveTo(220,30);
-pincel.lineTo(220,45);
-pincel.stroke();
-
-var botonJugar = document.querySelector("#boton-nuevoJuego");
-var botonDesistir = document.querySelector("#boton-desistir");
-var palabraAleatoria;
-var intentos = 6;
-botonJugar.onclick = empezarJuego;
-botonDesistir.disabled = true; 
-botonDesistir.onclick= terminarJuego;
-
-
-var listaPalabras= ["hola","jorge","pedro","piña","golpe","nahuelon","agua","salto","blanco","negro","ninja","magia","yana"];
-let cuerpo = document.querySelector("body")
-
+function empezarJuego(){
+    palabraAleatoria= listaPalabras[Math.floor(Math.random()*(listaPalabras.length-1))];
+    botonJugar.disabled = true;
+    botonRendirse.disabled = false;
+    avisoFinal.textContent ="";
+    pincel.clearRect(204,44,32,72);
+    intentos=6;
+    dibujarLineasPalabra(palabraAleatoria);
+    cuerpo.onkeydown = comprobarLetra;
+}
+function rendirse(){
+    avisoFinal.textContent = `Fin del juego, la palabra era ${palabraAleatoria}`;
+    cuerpo.onkeydown ="";
+    mostrarPalabra();
+    botonJugar.disabled = false;
+    botonRendirse.disabled = true;
+}
 function dibujarLineasPalabra(palabraAleatoria){
     var contenedorLetra = document.querySelector(".palabra-oculta");
     for(i = 0; i < palabraAleatoria.length ; i++){
@@ -46,39 +37,74 @@ function eliminarLineasPalabra(){
         let divHijo = document.querySelector(".letra");
         divHijo.remove();
     }
-    intentos= 6;
 }
-function colocarLetra(event){
+function dibujarCuerpo(parte){
+    pincel.beginPath();
+    switch (parte) {
+        case 5:
+            pincel.arc(220,55,10,0,2*3.14);
+            pincel.stroke();
+            break;
+        case 4:
+            pincel.moveTo(220,65);
+            pincel.lineTo(220,95);
+            pincel.stroke();
+            break;
+        case 3:
+            pincel.moveTo(220,65);
+            pincel.lineTo(205,85);
+            pincel.stroke();
+            break;
+        case 2:
+            pincel.moveTo(220,65);
+            pincel.lineTo(235,85);
+            pincel.stroke();
+            break;
+        case 1:
+            pincel.moveTo(220,95);
+            pincel.lineTo(205,115);
+            pincel.stroke();
+            break;
+        case 0:
+            pincel.moveTo(220,95);
+            pincel.lineTo(235,115);
+            pincel.stroke();
+            break;
+    }
+}
+function mostrarPalabra(){
+    for(i = 0; i < palabraAleatoria.length; i++){
+        let letraOculta = document.querySelector("#letra"+i);
+        letraOculta.style.visibility= 'visible';
+    }
+}
+function comprobarLetra(event){
     let acierto = false;
     for(i = 0; i < palabraAleatoria.length; i++){
-        var letraOculta = document.querySelector("#letra"+i);
-        var letraComparar = letraOculta.textContent;
+        let letraOculta = document.querySelector("#letra"+i);
+        let letraComparar = letraOculta.textContent;
         if (event.key.toUpperCase() == letraComparar){
             letraOculta.style.visibility= 'visible';
             acierto= true;
         }
     }
     if (!acierto){
-        console.log(intentos);
-        intentos-=1;
-        if (intentos==0){
-            alert(`Perdiste puerco desgraciado, la palabra oculta era ${palabraAleatoria}`);
-            terminarJuego();
+        if (comprobarDerrota()){
             return;
         }
     }
     comprobarVictoria();
 }
-function empezarJuego(){
-    palabraAleatoria= listaPalabras[Math.floor(Math.random()*(listaPalabras.length-1))];
-    botonJugar.disabled = true;
-    botonDesistir.disabled = false;
-    cuerpo.onkeydown = colocarLetra;
-    dibujarLineasPalabra(palabraAleatoria);
-}
-function terminarJuego(){
-    botonJugar.disabled = false;
-    eliminarLineasPalabra();
+function comprobarDerrota(){
+    intentos-=1;
+    dibujarCuerpo(intentos);
+    if (intentos==0){
+        avisoFinal.textContent = `Fin del juego, la palabra era ${palabraAleatoria}`;
+        cuerpo.onkeydown = "";
+        botonJugar.disabled = false;
+        botonRendirse.disabled = true;
+        return true;
+    }
 }
 function comprobarVictoria(){
     for(i=0; i<palabraAleatoria.length;i++){
@@ -87,5 +113,40 @@ function comprobarVictoria(){
             return;
         }
     }
-    alert(`Ganaste, la palabra era ${palabraAleatoria}`);
+    avisoFinal.textContent=`Ganaste, la palabra era ${palabraAleatoria}`;
+    cuerpo.onkeydown = "";
+    botonJugar.disabled = false;
+    botonRendirse.disabled= true;
 }
+
+
+let pantalla= document.querySelector("canvas");
+let pincel= pantalla.getContext("2d")
+pincel.fillStyle = "blue"
+pincel.strokeStyle = "blue";
+pincel.moveTo(90,130);
+pincel.lineTo(210,130);
+pincel.moveTo(150,130)
+pincel.lineTo(150,30);
+pincel.moveTo(150,30);
+pincel.lineTo(220,30);
+pincel.moveTo(220,30);
+pincel.lineTo(220,45);
+pincel.stroke();
+var botonJugar = document.querySelector("#boton-nuevoJuego");
+botonJugar.addEventListener("click",function(){
+    
+    eliminarLineasPalabra();
+    empezarJuego();
+});
+var botonRendirse = document.querySelector("#boton-desistir");
+botonRendirse.addEventListener("click",function(){
+    rendirse();
+});
+var avisoFinal = document.querySelector("#avisoMensaje");
+var cuerpo = document.querySelector("body")
+var palabraAleatoria;
+var intentos;
+var listaPalabras= ["hola","jorge","pedro","piña","golpe","nahuelon","agua","salto","blanco","negro","ninja","magia","yana"];
+
+empezarJuego();
